97d2fc1667e7c6a069a1ce3933037ad535de7802
Rahul Mitra <rmitra@trincoll.edu>
2019-03-15 16:00:16 -0400

    Class to store the score

---
+++ Score.java
@@ -0,0 +1,8 @@
+public class Score{
+    int score;//to store the score
+    Move someMove;//to store the move
+    public Score(int s, Move m){
+        this.score = s;
+        this.someMove = m;
+    }
+}
